a96f70cf24a19b75e63fe2ff8cace88e02616c43
Rakesh A <rakeshasokan06@gmail.com>
2022-03-16 12:52:39 +0530

    Updated Pages

--- src/App.js
+++ src/App.js
@@ -1,16 +1,32 @@
-import React from 'react';
-import './App.css';
-import Header from './Header';
+import React from "react";
+import "./App.css";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import Home from "./Home";
+import Header from "./Header";
+
+import Checkout from "./Checkout";
+import Login from "./Login";
 
 function App() {
   return (
-    //BEM
-    <div className="App">
-     <Header/>
-     {/* Home */}
+    <div className="app">
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/">
+            <Header />
+            <Home />
+          </Route>
+          <Route path="/checkout">
+            <Header />
+            <Checkout />
+          </Route>
 
+          <Route path="/login">
+            <Login />
+          </Route>
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
-
 export default App;
--- src/firebase.js
+++ src/firebase.js
@@ -1,4 +1,7 @@
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
+import firebase from 'firebase/compat'
+import 'firebase/firestore'
+
 const firebaseConfig = {
   apiKey: "AIzaSyDIpa9XGQmDzudM_YHotX9xd5K5rVNzJaM",
   authDomain: "clone-73789.firebaseapp.com",
@@ -8,3 +11,10 @@ const firebaseConfig = {
   appId: "1:830168362162:web:6e0676d81f4834158d6dab",
   measurementId: "G-CYRX96CY3E"
 };
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+
+export { db, auth };
